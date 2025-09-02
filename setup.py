d499b0a2cef34eb762e887a9b2263acb4953ceb7
Javad Dalirani <dalirani256@gmail.com>
2025-09-02 19:40:57 +0330

    Updated: setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aioprogress',
-    version='0.1.0',
+    version='0.1.1',
     description='A high-performance, flexible async file downloader built with aiohttp.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
