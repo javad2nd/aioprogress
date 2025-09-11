b91f00e58874675a2ef92a5889ea60b02a1560f5
Javad Dalirani <dalirani256@gmail.com>
2025-09-11 15:07:41 +0330

    version: 0.1.5.1

--- aioprogress/__init__.py
+++ aioprogress/__init__.py
@@ -4,7 +4,7 @@ Supports resume downloads, progress tracking, cancellation, proxy support,
 and concurrent downloads.
 """
 
-__version__ = "0.1.5"
+__version__ = "0.1.5.1"
 
 from .downloader import DownloadState, DownloadConfig, AsyncDownloader, DownloadManager
 from .progress import Progress, ProgressData
